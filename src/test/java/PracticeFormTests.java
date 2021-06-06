2899184ea9cccca28538318aa5dae74647164935
nazartal007 <a.nazarov@robo.finance>
2021-06-06 10:00:41 +0700

    update test with page and faker

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,47 +1,47 @@
-import com.codeborne.selenide.*;
-import org.junit.jupiter.api.BeforeAll;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import java.util.Locale;
 
 public class PracticeFormTests {
+    PracticeFormPage practiceFormPage = new PracticeFormPage();
+    Faker fakerRu = new Faker(new Locale("ru"));
+    Faker fakerEng = new Faker();
+
+    String firstName = fakerRu.name().firstName();
+    String lastName = fakerRu.name().lastName();
+    String email = fakerEng.internet().emailAddress();
+    String number = fakerRu.number().digits(10);
+    String address = fakerRu.address().fullAddress();
+    String state = "NCR";
+    String city = "Delhi";
+    String math = "Maths";
+    String male = "Male";
+    String hobbiesSport = "Sport";
+    String hobbiesReading = "Reading";
+    String hobbiesMusic = "Music";
 
     @Test
     void successfulSubmitForm() {
-        String firstName = "Привет";
-        String lastName = "Привет";
-        String email = "hello@guugle.com";
-        String number = "1234567890";
-        String address = "какой-то адрес";
-        String subjectsMaths = "Maths";
+        practiceFormPage.openFormPage();
+        practiceFormPage.setNameStudent(firstName, lastName);
+        practiceFormPage.setEmail(email);
+        practiceFormPage.chooseGenderMale();
+        practiceFormPage.setMobileNumber(number);
+        practiceFormPage.chooseSubject(math);
+        practiceFormPage.chooseHobbies();
+        practiceFormPage.setCurrentAddress(address);
+        practiceFormPage.choseStateAndCity(state, city);
+        practiceFormPage.submitForm();
 
-        open("https://demoqa.com/automation-practice-form");
+        practiceFormPage.assertStudentFullName(firstName, lastName);
+        practiceFormPage.assertStudentEmail(email);
+        practiceFormPage.assertGender(male);
+        practiceFormPage.assertMobNumber(number);
+        practiceFormPage.assertSubject(math);
+        practiceFormPage.assertHobbies(hobbiesSport, hobbiesReading, hobbiesMusic);
+        practiceFormPage.assertCurrentAddress(address);
+        practiceFormPage.assertStateAndCity(state, city);
 
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(email);
-        $("[for='gender-radio-1']").scrollTo().click();
-        $("#userNumber").setValue(number);
-        $("#subjectsInput").setValue("M");
-        $(byText(subjectsMaths)).click();
-        $("[for='hobbies-checkbox-1']").click();
-        $("#currentAddress").setValue(address);
-        $("#state").click();
-        $(byText("NCR")).click();
-        $("#city").click();
-        $(byText("Delhi")).click();
-        $("#submit").click();
-
-        $x("//*[@class='table-responsive']//*[text()='Student Name']/following::td").should(text(firstName + " " + lastName));
-        $x("//*[@class='table-responsive']//*[text()='Student Email']/following::td").should(text(email));
-        $x("//*[@class='table-responsive']//*[text()='Gender']/following::td").should(text("Male"));
-        $x("//*[@class='table-responsive']//*[text()='Mobile']/following::td").should(text(number));
-        $x("//*[@class='table-responsive']//*[text()='Date of Birth']/following::td").should(text("20 May,2021"));
-        $x("//*[@class='table-responsive']//*[text()='Subjects']/following::td").should(text(subjectsMaths));
-        $x("//*[@class='table-responsive']//*[text()='Hobbies']/following::td").should(text("Sports"));
-        $x("//*[@class='table-responsive']//*[text()='Address']/following::td").should(text(address));
-        $x("//*[@class='table-responsive']//*[text()='State and City']/following::td").should(text("NCR Delhi"));
     }
 }
